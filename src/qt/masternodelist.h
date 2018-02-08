732438bdf2fc177ac5e415bd906bdf64854b99fb
philscurrency <philscurrency@gmail.com>
2018-02-08 14:53:51 +0000

    QTableWidget fixes

--- src/qt/masternodelist.h
+++ src/qt/masternodelist.h
@@ -67,6 +67,7 @@ private:
 
 private Q_SLOTS:
     void showContextMenu(const QPoint &);
+    void on_filterLineEdit_textChanged(const QString& strFilterIn);
     void on_startButton_clicked();
     void on_startAllButton_clicked();
     void on_startMissingButton_clicked();
